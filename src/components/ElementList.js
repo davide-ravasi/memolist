01cf81de07b40b1829d1ae67e04b08c8da317980
davide <info@davideravasi.com>
2020-11-24 16:15:32 +0000

    refs #9 add list compo with css

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { db } from '../firebase/config';
+import ElementDetails from './ElementDetails'; 
 
 const ElementList = () => {
     useEffect(() => {
@@ -10,7 +11,22 @@ const ElementList = () => {
         });
     }, []);
     
-    return <div>List</div>
+    return (
+        <div className="max-w-screen-lg mx-auto">
+            <div className="grid grid-cols-4 gap-4 py-8">
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+                <ElementDetails />
+            </div>
+        </div>
+    )
 }
 
 export default ElementList;
