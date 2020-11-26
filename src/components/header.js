9f561f3fd32a242aa4fe6ffa2293091e1decbb76
davide <info@davideravasi.com>
2020-11-26 17:16:41 +0000

    refs #16 added logo icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { faThList } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
     const menuItemsCss = 'px-6 py-1 text-white font-medium text-base hover:bg-gray-600 rounded-md';
@@ -6,7 +8,7 @@ const Header = () => {
         <div className="containe bg-gray-800 shadow-md">
             <div className="mx-auto max-w-screen-lg flex h-16 flex items-center justify-between h-16">
                 <div>
-                    <a className="logo px-2">Logo</a>
+                    <a className="logo px-2 text-white text-xl mt-6 text-indigo-300"><FontAwesomeIcon icon={faThList} /></a>
                     <a href="#" className={menuItemsCss}>Home</a>
                     <a href="#" className={menuItemsCss}>All Lists</a>
                     <a href="#" className={menuItemsCss}>Categories</a>
