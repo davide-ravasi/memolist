c43834aa68309296507c2d3be71247ebefb4762b
davide <info@davideravasi.com>
2020-12-02 14:31:58 +0000

    refs #18 add google auth conn

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,32 +1,34 @@
-import React, { useEffect } from 'react';
-import { db } from '../firebase/config';
-import ElementDetails from './ElementDetails'; 
+import React, { useEffect } from "react";
+import { db } from "../firebase";
+import ElementDetails from "./ElementDetails";
 
 const ElementList = () => {
-    useEffect(() => {
-        db.collection('items').get().then((querySnapshot) => {
-            querySnapshot.forEach(function(doc) {
-                console.log(doc.id, " => ", doc.data());
-            });
+  useEffect(() => {
+    db.collection("items")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(function (doc) {
+          console.log(doc.id, " => ", doc.data());
         });
-    }, []);
-    
-    return (
-        <div className="max-w-screen-lg mx-auto">
-            <div className="grid grid-cols-4 gap-4 py-8">
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-                <ElementDetails />
-            </div>
-        </div>
-    )
-}
+      });
+  }, []);
+
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <div className="grid grid-cols-4 gap-4 py-8">
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+        <ElementDetails />
+      </div>
+    </div>
+  );
+};
 
 export default ElementList;
