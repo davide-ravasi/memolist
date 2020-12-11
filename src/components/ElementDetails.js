269986935384d9846bb748217b377a3b8dd522c9
davide <info@davideravasi.com>
2020-12-11 14:40:31 +0000

    refs #23-connect-list-redux-display-home

--- src/components/ElementDetails.js
+++ src/components/ElementDetails.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { text } from '@fortawesome/fontawesome-svg-core';
 
-const ElementDetails = () => {
+const ElementDetails = (props) => {
+    const {id, status, userId, color, category, link, text, name} = props;
     return (
-        <div className="bg-white rounded-md p-3 shadow-xl relative">
-                <div className="text-gray-900 text-xl">Name 1</div>
-                <span className="text-gray-600 bg-gray-100 uppercase shadow-xl px-1 text-xs rounded">Category 1</span>
-                <div className="text-gray-400 text-sm">Description descriptions......</div>
-                <a href="#" className="flex justify-center items-center text-white text-sm absolute top-3 right-2 rounded-full h-6 w-6 bg-gray-400 hover:bg-gray-700 transition duration-500 ease-in-out">
-                    <FontAwesomeIcon icon={faLink} />
-                </a>
+        <div key={id} className="bg-white rounded-md p-3 shadow-xl relative">
+            <div className="text-gray-900 text-xl">{name}</div>
+            <span className="text-gray-600 bg-gray-100 uppercase shadow-xl px-1 text-xs rounded">{category}</span>
+            <div className="text-gray-400 text-sm">{text}</div>
+            <Link to={link} className="flex justify-center items-center text-white text-sm absolute top-3 right-2 rounded-full h-6 w-6 bg-gray-400 hover:bg-gray-700 transition duration-500 ease-in-out">
+                <FontAwesomeIcon icon={faLink} />
+            </Link>
         </div>
     )
 }
