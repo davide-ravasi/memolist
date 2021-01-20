98533600133cdc55a9e764fc3e668d81ccd2c8df
davide <info@davideravasi.com>
2021-01-20 14:51:28 +0000

    refs #38-add-cat-list-on-homepage-and-#36-btn-create-el

--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -1,14 +1,16 @@
-import { FETCH_CATEGORIES } from "../actions/types";
+import { FETCH_CATEGORIES, SET_ACTIVE_CATEGORY } from "../actions/types";
 
 const initial_state = {
-    listCategories: []
+    listCategories: [],
+    activeCategory: ''
 }
 
 export default (state = initial_state, action) => {
-    console.log('action in lisr reducer', action);
     switch(action.type) {
         case FETCH_CATEGORIES:
             return {...state, listCategories: action.payload}
+        case SET_ACTIVE_CATEGORY:
+            return {...state, activeCategory: action.payload}    
         default:
             return state
     }
