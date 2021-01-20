f0ed8d7c040ee8b8a3ee0dfd650dc619dc8acb37
davide <info@davideravasi.com>
2021-01-20 16:30:49 +0000

    refs #43 added custom color cat

--- src/components/ElementDetails.js
+++ src/components/ElementDetails.js
@@ -6,11 +6,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import { text } from '@fortawesome/fontawesome-svg-core';
 
 const ElementDetails = (props) => {
-    const {id, status, userId, color, category, link, text, name} = props;
+    const {id, status, userId, color, category, link, text, name, categories} = props;
+    const cat = categories.find(cat => {
+        return cat.name === category
+    });
+
     return (
         <div key={id} className="bg-white rounded-md p-3 shadow-xl relative">
             <div className="text-gray-900 text-xl">{name}</div>
-            <span className="text-gray-600 bg-gray-100 uppercase shadow-xl px-1 text-xs rounded">{category}</span>
+            <span className="text-gray-600 bg-gray-100 uppercase shadow-xl px-1 text-xs rounded" style={cat ? {backgroundColor: cat.color} : {}}>{category}</span>
             <div className="text-gray-400 text-sm">{text}</div>
             <Link to={link} className="flex justify-center items-center text-white text-sm absolute top-3 right-2 rounded-full h-6 w-6 bg-gray-400 hover:bg-gray-700 transition duration-500 ease-in-out">
                 <FontAwesomeIcon icon={faLink} />
