180bb62f58896e0c48419154ad2bd30fad954193
davide <info@davideravasi.com>
2021-01-27 17:13:24 +0000

    add created_at in element and function outils for date convertion from timestamp

--- src/components/ElementDetails.js
+++ src/components/ElementDetails.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { convertDateFromTimestamp } from '../outils/dateConverter';
+
 import { faLink, faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import { text } from '@fortawesome/fontawesome-svg-core';
 
 const ElementDetails = (props) => {
-    const {id, status, userId, color, category, link, text, name, categories} = props;
+    console.log(props);
+    const {id, status, userId, color, category, link, text, name, categories, created_at} = props;
+    const createdAtConverted = convertDateFromTimestamp(created_at);
+
+
     const cat = categories.find(cat => {
         return cat.name === category
     });
@@ -15,7 +21,8 @@ const ElementDetails = (props) => {
         <div key={id} className="bg-white rounded-md p-3 shadow-xl relative">
             <div className="text-gray-900 text-xl">{name}</div>
             <span className="text-gray-600 bg-gray-100 uppercase shadow-xl px-1 text-xs rounded" style={cat ? {backgroundColor: cat.color} : {}}>{category}</span>
-            <div className="text-gray-400 text-sm">{text}</div>
+            <div className="text-gray-500 text-sm">{text}</div>
+            <div className="text-gray-400 text-xs pt-2">Created: {createdAtConverted}</div>
             <Link to={link} className="flex justify-center items-center text-white text-sm absolute top-3 right-2 rounded-full h-6 w-6 bg-gray-400 hover:bg-gray-700 transition duration-500 ease-in-out">
                 <FontAwesomeIcon icon={faLink} />
             </Link>
