e56cab97dcea5958697d801172fe8d2305c2905b
davide <info@davideravasi.com>
2021-02-05 10:14:43 +0000

    refs #55#50-refactor-list-element-state-add-modal-portal-create-action-remove-element

--- src/components/ElementList.js
+++ src/components/ElementList.js
@@ -1,32 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
-import ElementDetails from "./ElementDetails";
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchList } from '../actions/';
-import CategoryList from "./CategoryList";
+import { fetchList, removeElement } from '../actions/';
 
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Modal from './ModalPortal';
+import ConfirmModal from './ConfirmModal';
+import CategoryList from "./CategoryList";
+import ElementDetails from "./ElementDetails";
 
 const ElementList = (props) => {
   const selectEls = state => state.list;
-  const elements = useSelector(selectEls);
+  const {listItems} = useSelector(selectEls);
   const selectCats = state => state.categories;
   const {listCategories, activeCategory} = useSelector(selectCats);
   const dispatch = useDispatch();
+
+  const [modalShow, setModalShow] = useState(false);
+  const [idElToRemove, setIdElToRemove] = useState('');
+
   const stylesBtnAdd = `flex justify-center items-center text-white 
                         text-sm absolute right-2 rounded-full h-8 w-8 
                         bg-green-600 hover:bg-green-800 transition duration-500 ease-in-out`;
 
-  const filteredEls = activeCategory ? elements.filter(el => el.category === activeCategory) : elements
+  const filteredEls = activeCategory ? listItems.filter(el => el.category === activeCategory) : listItems
+
+  const onRemove = (id) => {
+    setModalShow(true);
+    setIdElToRemove(id);
+  }
+
+  const onCloseModal = () => {
+    setModalShow(false);
+    setIdElToRemove('');
+  }
+
+  const onConfirmModal = () => {
+    setModalShow(false);
+    dispatch(removeElement(idElToRemove));
+    console.log('dispatch remove action with id: ', idElToRemove);
+  }
 
   useEffect(() => {
     dispatch(fetchList());
   }, []);
 
   return (
+    <>
     <div className="max-w-screen-lg mx-auto">
       <div className="grid grid-cols-4 gap-4 my-8 relative">
         <CategoryList />
@@ -35,13 +58,17 @@ const ElementList = (props) => {
         </Link>
       </div>
       <div className="grid grid-cols-4 gap-4 py-8">
-        { elements.length ?
-          filteredEls.map(el => <ElementDetails {...el} categories={listCategories}  />)
+        { listItems.length ?
+          filteredEls.map(el => <ElementDetails {...el} categories={listCategories} onRemove={onRemove} />)
           :
           <div>Loading...</div>
         }
       </div>
     </div>
+    <Modal>
+      <ConfirmModal modalShow={modalShow} closeModal={onCloseModal} confirmModal={onConfirmModal} />
+    </Modal>
+    </>
   );
 };
 
