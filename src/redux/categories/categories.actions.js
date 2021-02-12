e717a12016125dddadd366a768e896278337a7a7
Davide Ravasi <davide.ravasi@brandalley.com>
2021-02-12 15:05:25 +0100

    refs #62 gestione errori nei reducers

--- src/redux/categories/categories.actions.js
+++ src/redux/categories/categories.actions.js
@@ -1,14 +1,18 @@
 import { FETCH_CATEGORIES, SET_ACTIVE_CATEGORY } from "./categories.types";
+import { ERROR_MESSAGE } from '../system/system.types';
 import { db } from "../../firebase";
 
 
 export const fetchCategories = () => async dispatch => {
+  try {
     const listCategories = await db.collection('categories').get();
     const arrCat = await listCategories.docs.map(cat => cat.data());
-  
     dispatch({type: FETCH_CATEGORIES, payload: arrCat});
+  } catch(err) {
+    dispatch({ type: ERROR_MESSAGE, payload: {...err} });
   }
+}
   
-  export const setActiveCategory = (cat) => {
-    return {type: SET_ACTIVE_CATEGORY, payload: cat}
-  }
+export const setActiveCategory = (cat) => {
+  return {type: SET_ACTIVE_CATEGORY, payload: cat}
+}
--- src/redux/list/list.actions.js
+++ src/redux/list/list.actions.js
@@ -1,26 +1,44 @@
 import { FETCH_LIST, ADD_ELEMENT, EDIT_ELEMENT, REMOVE_ELEMENT, CLEAN_FEEDBACK_MSG } from "./list.types";
+import { ERROR_MESSAGE } from '../system/system.types';
 import { db } from "../../firebase";
 
 export const fetchList = () => async function(dispatch) {
-  const list = await db.collection('notes').get();
-  // add doc id to every element in collection
-  const arrayList = list.docs.map(doc => { return {...doc.data(), id: doc.id}});
-  dispatch({ type: FETCH_LIST, payload: arrayList });
+  try {
+    const list = await db.collection('notes').get();
+    // add doc id to every element in collection
+    const arrayList = list.docs.map(doc => { return {...doc.data(), id: doc.id}});
+    dispatch({ type: FETCH_LIST, payload: arrayList });
+  } catch(err) {
+    dispatch({ type: ERROR_MESSAGE, payload: {...err} });
+  }
+  
 }
 
 export const addElement = (el, name) => async dispatch => {
-  const addEl = await db.collection('notes').add({...el, userName: name, created_at: new Date()});
-  dispatch({ type: ADD_ELEMENT, payload: el});
+  try {
+    const addEl = await db.collection('notes').add({...el, userName: name, created_at: new Date()});
+    dispatch({ type: ADD_ELEMENT, payload: el});
+  } catch(err) {
+    dispatch({ type: ERROR_MESSAGE, payload: {...err} });
+  }
 }
 
 export const editElement = (el) => async dispatch => {
-  const editEl = await db.collection('notes').doc(el.id).set(el);
-  dispatch({ type: EDIT_ELEMENT, payload: el});
+  try {
+    const editEl = await db.collection('notes').doc(el.id).set(el);
+    dispatch({ type: EDIT_ELEMENT, payload: el});
+  } catch(err) {
+    dispatch({ type: ERROR_MESSAGE, payload: {...err} });
+  }
 }
 
 export const removeElement = (el) => async dispatch => {
-  const removeEl = await db.collection('notes').doc(el.id).delete();
-  dispatch({ type: REMOVE_ELEMENT, payload: el});
+  try {
+    const removeEl = await db.collection('notes').doc(el.id).delete();
+    dispatch({ type: REMOVE_ELEMENT, payload: el});
+  } catch(err) {
+    dispatch({ type: ERROR_MESSAGE, payload: {...err} });
+  }
 }
 
 export const cleanFeedbackMsg = () => (
