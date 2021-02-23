af6e362b93a8e74e04e5caeeca768e1d3dff78eb
davide <info@davideravasi.com>
2021-02-23 10:35:35 +0100

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBKnRhpd3uGThHKlGVktVby5FPdBCz1XCQ",
+    authDomain: "todoapp-2c6e5.firebaseapp.com",
+    databaseURL: "https://todoapp-2c6e5.firebaseio.com",
+    projectId: "todoapp-2c6e5",
+    storageBucket: "todoapp-2c6e5.appspot.com",
+    messagingSenderId: "831096100566",
+    appId: "1:831096100566:web:b877c07f2665e25d835b77",
+  };
