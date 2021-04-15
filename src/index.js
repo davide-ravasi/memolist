61c1d7654be935c5f05bbdbe75e013f2769f84cb
davide <info@davideravasi.com>
2021-04-15 10:57:09 +0000

    refs #96 added save load state from local storage and refactor on external file

--- src/index.js
+++ src/index.js
@@ -7,25 +7,6 @@ import store from "./redux/store";
 
 import App from "./App";
 
-const saveState = (state) => {
-  try {
-    const serialState = JSON.stringify(state);
-    localStorage.setItem("appState", serialState);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-store.subscribe(() => {
-  saveState({
-    /* example state */
-    categories: store.getState().categories,
-    list: store.getState().list,
-    system: store.getState().system,
-    wishlist: store.getState().wishlist,
-  });
-});
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
