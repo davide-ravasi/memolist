af0f83fb92874d705acb160bdd46403b3606ada0
davide <info@davideravasi.com>
2021-04-16 16:27:20 +0000

    refs #11 added external library fontaewsome

---
+++ src/outils/fontAwesome.js
@@ -0,0 +1,18 @@
+// import the library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import {
+  faLink,
+  faPen,
+  faTimes,
+  faHeart,
+  faCopy,
+  faPlus,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faLink, faPen, faTimes, faHeart, faCopy, faPlus, faUser.icon);
+
+// https://towardsdatascience.com/build-a-stunning-readme-for-your-github-profile-9b80434fe5d7
+// https://fontawesome.com/how-to-use/on-the-web/using-with/react
