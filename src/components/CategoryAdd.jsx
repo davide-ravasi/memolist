1abb10e0e29cd63f85f082f7637c89c2d2121458
davide <info@davideravasi.com>
2021-04-28 15:34:13 +0000

    refs #111 add new cat form and ui

---
+++ src/components/CategoryAdd.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import { useDispatch } from "react-redux";
+
+const resetValues = {
+  name: "",
+};
+
+const CategoryForm = () => {
+  const dispatch = useDispatch();
+
+  const labelStyles = "block text-md text-center text-gray-7000 pt-2 pb-1";
+  const inputStyles =
+    "w-full shadow-md p-1.5 text-sm border border-solid border-gray-300 rounded-lg";
+  const error = "block text-sm text-red-600 pt-1";
+  let initialValues = resetValues;
+  const requiredMsg = "This field is required";
+
+  // Synchronous validation
+  const validate = (values, props) => {
+    const errors = {};
+
+    if (!values.name) {
+      errors.name = requiredMsg;
+    } else if (values.name.length < 4) {
+      errors.name = "At least 4 characters";
+    }
+
+    return errors;
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        dispatch();
+      }}
+      validate={validate}
+    >
+      {({ errors, touched }) => (
+        <Form className="max-w-md mx-auto">
+          <label htmlFor="name" className={labelStyles}>
+            Category name
+          </label>
+          <Field
+            id="name"
+            name="name"
+            placeholder="Add the new category name"
+            className={inputStyles}
+          />
+          {errors.name && touched.name ? (
+            <div className={error}>{errors.name}</div>
+          ) : null}
+          <div className="mt-4 flex justify-center content-center">
+            <button
+              type="submit"
+              className="p-1.5 rounded-lg bg-blue-600 text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default CategoryForm;
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import EdiText from "react-editext";
 
-import { modifyCategory } from '../redux/categories/categories.actions';
+import { modifyCategory } from "../redux/categories/categories.actions";
+
+import CategoryForm from "../components/CategoryAdd";
 
 const Categories = () => {
   const { listCategories } = useSelector((state) => state.categories);
+  const [showAddForm, setShowAddForm] = useState(false);
   const dispatch = useDispatch();
 
   const roundbtnStyles =
     "flex justify-center items-center text-white text-sm rounded-full h-6 w-6 transition duration-500 ease-in-out";
+
+  const stylesBtnAdd = `absolute right-0 top-3 flex justify-center items-center text-white 
+    text-sm absolute right-2 rounded-full h-8 w-8 
+    bg-green-600 hover:bg-green-800 transition duration-500 ease-in-out`;
 
   const editBtnStyles = `${roundbtnStyles} bg-gray-400 hover:bg-green-700`;
 
@@ -19,14 +26,29 @@ const Categories = () => {
   const saveBtnStyles = `${roundbtnStyles} bg-green-400 hover:bg-green-700`;
 
   const onSave = (idCat, newName) => {
-    console.log("idCat ", idCat);
-    console.log("Edited Value -> ", newName);
     dispatch(modifyCategory(idCat, newName));
   };
 
+  const onClick = () => {
+    setShowAddForm(true);
+  };
+
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-lg mx-auto relative">
       <h1 className="mt-8 mx-4 font-md text-2xl">Manage your Categories</h1>
+      <button
+        type="button"
+        className={stylesBtnAdd}
+        title="Add a new category"
+        onClick={onClick}
+      >
+        <FontAwesomeIcon icon="plus" />
+      </button>
+      {showAddForm && (
+        <div className="bg-gray-300 border shadow-xl rounded-md my-5 py-4">
+          <CategoryForm />
+        </div>
+      )}
       <div className="mt-8 grid md:grid-cols-3 xs:grid-cols-1 gap-4 relative">
         {listCategories &&
           listCategories.map((cat) => {
