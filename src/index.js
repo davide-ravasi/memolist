7083f50c34abddcb24886c4bb8f7fffb2a5dff22
davide <info@davideravasi.com>
2021-06-23 15:13:19 +0000

    refs changed name service worker file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import register from "./serviceWorker";
+import register from "./service-worker";
 
 import store from "./redux/store";
 
