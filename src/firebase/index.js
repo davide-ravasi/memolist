b2192073d4631c52e585e1df047a1edf5ef481ef
davide <info@davideravasi.com>
2021-07-13 14:30:38 +0000

    refs #135-enabling-data-firestore-persistence-when-offline

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,10 +6,23 @@ import { ERROR_MESSAGE } from "../redux/system/system.types";
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+
 // init firestore and auth
 // refs: https://firebase.google.com/docs/web/setup
 export const db = firebase.firestore();
 export const auth = firebase.auth();
+
+
+// enable firestore persistence
+// when app is offline
+firebase.firestore().enablePersistence()
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      console.log("Multiple tabs open, persistence can only be enabled in one tab at a a time");
+    } else if (err.code == 'unimplemented') {
+      console.log("The current browser does not support all of the features required to enable persistence");
+    }
+  });
 
 // connect with google popup
 // https://firebase.google.com/docs/auth/web/google-signin
@@ -18,55 +31,55 @@ export const signInGoogle = () => firebase.auth().signInWithPopup(provider);
 
 export const signInWithEmailAndPassword = async (email, password) => {
   try {
-     await firebase.auth().signInWithEmailAndPassword(email, password)
-  } catch(err) {
-    store.dispatch({type: ERROR_MESSAGE, payload: {...err}});
+    await firebase.auth().signInWithEmailAndPassword(email, password)
+  } catch (err) {
+    store.dispatch({ type: ERROR_MESSAGE, payload: { ...err } });
   }
 }
 
 export const manageUserData = async (user) => {
 
-    if(!user) return false;
+  if (!user) return false;
 
-    const userRef = await db.collection('users').doc(user.uid);
-    const userDoc = await userRef.get();
+  const userRef = await db.collection('users').doc(user.uid);
+  const userDoc = await userRef.get();
 
-    if(!userDoc.exists) {
-      const { uid, email, displayName: name, photoURL } = user;
-      userRef.set({
-        uid,
-        name,
-        email,
-        photoURL,
-        createdAt: new Date(),
-        role: 'reader'
-      });
-    }
+  if (!userDoc.exists) {
+    const { uid, email, displayName: name, photoURL } = user;
+    userRef.set({
+      uid,
+      name,
+      email,
+      photoURL,
+      createdAt: new Date(),
+      role: 'reader'
+    });
+  }
 
-    if(userDoc.exists) {
-      const { uid, email, name, role, createdAt, photoURL } = userDoc.data();
-      userRef.set({
-        uid,
-        name,
-        email,
-        createdAt,
-        photoURL,
-        role
-      });
-    }
+  if (userDoc.exists) {
+    const { uid, email, name, role, createdAt, photoURL } = userDoc.data();
+    userRef.set({
+      uid,
+      name,
+      email,
+      createdAt,
+      photoURL,
+      role
+    });
+  }
 
-    return userDoc.data();
+  return userDoc.data();
 };
 
 export const checkIsAdmin = async (currentUser) => {
-  if(!currentUser) return false;
+  if (!currentUser) return false;
 
   const userRef = await db.collection('users').doc(currentUser.uid);
   const userDoc = await userRef.get();
 
-  if(userDoc.exists && userDoc.data().role === 'admin') {
+  if (userDoc.exists && userDoc.data().role === 'admin') {
     return true;
   }
-  
+
   return false;
 }
