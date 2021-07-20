4305836bbed64e9f06de2dbe490b1f8d6465e52c
davide <info@davideravasi.com>
2021-07-20 13:49:34 +0000

    refactor RouteChangeTracker component

--- src/components/RouteChangeTracker.jsx
+++ src/components/RouteChangeTracker.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { withRouter } from 'react-router-dom';
 import ReactGA from 'react-ga';
 const RouteChangeTracker = ({ history }) => {
-
-    history.listen((location, action) => {
-        ReactGA.set({ page: location.pathname });
-        ReactGA.pageview(location.pathname);
-    });
+    const {location} = history;
+    useEffect(() => {
+            console.log(location.pathname);
+            ReactGA.set({ page: location.pathname });
+            ReactGA.pageview(location.pathname);
+    },[location])
 
     return <div></div>;
 };
