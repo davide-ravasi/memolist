7510b9e2a77d963e573e96ebb0fb22b23ef20acc
davide <info@davideravasi.com>
2021-10-08 09:24:25 +0000

    fixed warning that fails build

--- src/redux/flashcard/flashcard.actions.js
+++ src/redux/flashcard/flashcard.actions.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase';
-import { ADD_FLASHCARD } from './flashcard.types';
+// import firebase from 'firebase';
+// import { ADD_FLASHCARD } from './flashcard.types';
 import { db } from '../../firebase/index';
 
 export const addFlashCard = (el, description) => async (dispatch) => {
@@ -7,12 +7,12 @@ export const addFlashCard = (el, description) => async (dispatch) => {
   console.log(description);
 
   try {
-    const docRef = await db.collection("flashcards").add({
+    await db.collection("flashcards").add({
       ...el,
       description
     })
 
-    
+
 
   } catch (err) {
     console.log(err);
